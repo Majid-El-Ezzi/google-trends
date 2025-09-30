cc8ce3446d57431d40c62f11f946c56d431c73ee
Majid-El-Ezzi <majid.elezzi@lau.edu>
2025-09-30 11:46:42 +0300

    Update db.py
    Get the DB URL from environment variable (set in docker-compose.yml)

--- app/db.py
+++ app/db.py
@@ -1,8 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-db_url="postgresql+psycopg2://majid:majid@localhost:5432/trends"
+db_url= os.getenv("DATABASE_URL", "postgresql+psycopg2://majid:majid@localhost:5432/trends")
 
 engine = create_engine(db_url)
 Base = declarative_base()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base.metadata.create_all(bind=engine)
