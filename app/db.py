ba134c71d9f212caace729bf520d58c4f1f09369
Majid-El-Ezzi <majid.elezzi@lau.edu>
2025-09-30 15:23:37 +0300

    Update db.py

--- app/db.py
+++ app/db.py
@@ -7,4 +7,3 @@ db_url= os.getenv("DATABASE_URL", "postgresql+psycopg2://majid:majid@localhost:5
 engine = create_engine(db_url)
 Base = declarative_base()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base.metadata.create_all(bind=engine)
