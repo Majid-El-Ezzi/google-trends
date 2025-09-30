22f4f3e845563aadcdc4f84608642b94eae335c0
Majid-El-Ezzi <majid.elezzi@lau.edu>
2025-09-30 15:23:54 +0300

    Update init_db.py

--- app/init_db.py
+++ app/init_db.py
@@ -2,5 +2,11 @@ from db import Base, engine
 from models_db import Trend
 
 # Create the table in the database
-Base.metadata.create_all(bind=engine)
-print("Database initialized and table created.")
+def init_db():
+    print("Initializing database...")
+    # Create all tables defined in models.py
+    Base.metadata.create_all(bind=engine)
+    print("Database initialized.")
+
+if __name__ == "__main__":
+    init_db()
