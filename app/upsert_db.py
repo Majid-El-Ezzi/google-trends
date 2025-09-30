32f1ce6e9ef88959a73e9b507f8104d91befc450
Majid-El-Ezzi <majid.elezzi@lau.edu>
2025-09-30 10:49:08 +0300

    Changed the upsert function
    - Implemented batch processing in upsert function
    - Splits records into chunks of 50 for insertion/update
    -Improves performance on large datasets
    - Keeps same upsert logic with conflict resolution on (date, keyword, geo)

--- app/upsert_db.py
+++ app/upsert_db.py
@@ -2,24 +2,27 @@ from sqlalchemy.dialects.postgresql import insert
 from db import SessionLocal
 from models_db import Trend
 
-def upsert_trends(records):
+        
+def upsert_trends(records, batch_size=50):
     session = SessionLocal()
     try:
-        for record in records: # this function is upserting row by row. Not efficient for large datasets.
-            query = insert(Trend).values(
-                date=record.date,
-                keyword=record.keyword,
-                geo=record.geo,
-                interest=record.interest
-            ).on_conflict_do_update( # CHeck for conflict based on unique constraint
-                index_elements=['date', 'keyword', 'geo'], 
-                set_=dict(interest=record.interest) # update interest if conflict occurs
-            )
-            session.execute(query) #execute the above statement.
-        session.commit() #commit changes
-        print(f"Upserted {len(records)} records successfully.")
+        for i in range(0, len(records), batch_size):
+            batch = records[i:i+batch_size]
+            for record in batch:
+                query = insert(Trend).values(
+                    date=record.date,
+                    keyword=record.keyword,
+                    geo=record.geo,
+                    interest=record.interest
+                ).on_conflict_do_update(# CHeck for conflict based on unique constraint
+                    index_elements=['date', 'keyword', 'geo'],
+                    set_=dict(interest=record.interest)# update interest if conflict occurs
+                )
+                session.execute(query) #execute the above statement.
+            session.commit() #commit changes
+            print(f"Upserted batch {i//batch_size + 1} ({len(batch)} records).")
     except Exception as e:
         session.rollback()
-        print("Error during upsert:", e)
+        print("Error during batch upsert:", e)
     finally:
         session.close()
